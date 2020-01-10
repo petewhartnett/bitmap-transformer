85f4981400a2c290e3b8dd10a9b98dfd5e1b3e50
Chase Tucker <tuck8106@gmail.com>
2020-01-10 10:14:05 -0800

    able to read image

--- src/main/java/bitmap/transformer/Bitmap.java
+++ src/main/java/bitmap/transformer/Bitmap.java
@@ -1,7 +1,24 @@
 package bitmap.transformer;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Bitmap {
 
+  BufferedImage img = null;
+
+  public void readBitmap(File path) {
+
+    try {
+      this.img = ImageIO.read(path);
+      System.out.println(this.img);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("can't read file");
+    }
+  }
 
 
 
